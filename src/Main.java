7e6a0f32825e0dadc7bd11e6cfc325d604747030
owner90 <owner90@owner90-PC>
2016-10-09 13:52:46 +0300

    111

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,19 @@
-
+import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int num = 1;
+		System.out.println("Insert number: ");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		while (num <= n) {
+			if (num % 2 != 0) {
+				System.out.println(num);
+			}
+			num++;
+		}
+		sc.close();
 
 	}
 
